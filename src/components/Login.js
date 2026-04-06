53f96ae8cdb7658ae5f6bcea8b0ac4ab265c3381
kiyo4810 <kiyo4810@gmail.com>
2026-04-06 22:28:01 +0900

    google Auth installed

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,30 @@
+import { signInWithPopup } from 'firebase/auth';
 import React from 'react';
+import { auth, provider } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
-  return <div>Login</div>;
+const Login = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+  const loginInWithGoogle = () => {
+    // googleでログイン
+    // Login.js の loginInWithGoogle 関数内
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log('ログイン成功！', result.user); // これが出るかチェック
+        localStorage.setItem('isAuth', 'true');
+        setIsAuth(true);
+        navigate('/');
+      })
+      .catch((error) => {
+        console.error('ログイン失敗...', error.message); // エラーが出れば原因がわかる
+      });
+  };
+  return (
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginInWithGoogle}>Google でログイン</button>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,7 +1,26 @@
+import { signInWithPopup, signOut } from 'firebase/auth';
 import React from 'react';
+import { auth, provider } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
-const Logout = () => {
-  return <div>Logout</div>;
+const Logout = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+  const logout = () => {
+    // googleでログアウト
+    // signInWithPopup(auth, provider).then((result) => {
+    signOut(auth).then(() => {
+      // console.log(result);
+      localStorage.clear();
+      setIsAuth(false);
+      navigate('/login');
+    });
+  };
+  return (
+    <div>
+      <p>ログアウトする</p>
+      <button onClick={logout}>ログアウト</button>
+    </div>
+  );
 };
 
 export default Logout;
